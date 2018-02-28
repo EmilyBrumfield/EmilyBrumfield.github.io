677627de251d11daeffd2bef650b451ab4a24d64
Emily Brumfield <belac54@hotmail.com>
2018-02-28 10:14:38 -0500

    add service worker, PWA icons

--- anarchy-roller/anarchy-roller.js
+++ anarchy-roller/anarchy-roller.js
@@ -1,3 +1,15 @@
+// ServiceWorker is a progressive technology. Ignore unsupported browsers
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register('service-worker.js').then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
+
 /*SHADOWRUN ANARCHY DICE ROLLER
 Progressiva web app that rolls Shadowrun Anarchy dice without hassle or unnecessary clicks.
 
